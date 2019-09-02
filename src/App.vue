ef3ef33f8ae0c0f5db846244f4e64af1e2e0a020
Petrenko Konstantin <kpetrenko1@gmail.com>
2019-09-02 08:52:22 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,161 @@
+<template>
+    <nortex-table
+        api_url="http://brokeridge.loc:3000/admin/users"
+        :columns='columns'
+        :localization="localization"
+    ></nortex-table>
+</template>
+
+
+
+<script>
+    import NortexTable from './NortexTable';
+    import 'bootstrap/dist/css/bootstrap.css'
+    import axios from 'axios'
+    window.axios = axios;
+    axios.defaults.headers.common['X-CSRF-Token'] = "SSmtWGksGDIoVM9f8z8EhZFkifjHAR14Su7MHdjfeEHthVg8sj/jD1JXr5nNaSCsqKW5g3sJnod/jugP7Zfwig==";
+    axios.defaults.headers.common['Accept'] = 'application/json';
+    import VTooltip from 'v-tooltip'
+    export default {
+        name: 'App',
+        components: {NortexTable},
+        data(){
+            return {
+                columns: [
+                    {'field':'email','label':'E-mail','title':false,'sortable':true,'html':true,'rowspan':null,'colspan':null,'class':{'sorting':true,'asc':false,'desc':false}},
+                    {'field':'firstname','label':'First Name','title':false,'sortable':true,'html':true,'rowspan':null,'colspan':null,'class':{'sorting':true,'asc':false,'desc':false}},
+                    {'field':'surname','label':'Surname','title':false,'sortable':true,'html':true,'rowspan':null,'colspan':null,'class':{'sorting':true,'asc':false,'desc':false}},
+                    {'field':'patronymic','label':'Patronymic','title':false,'sortable':true,'html':true,'rowspan':null,'colspan':null,'class':{'sorting':true,'asc':false,'desc':false}},
+                    {'field':'phone','label':'Phone','title':false,'sortable':true,'html':true,'rowspan':null,'colspan':null,'class':{'sorting':true,'asc':false,'desc':false}},
+                    {'field':'verified','label':'Verified','title':false,'sortable':true,'html':true,'rowspan':null,'colspan':null,'class':{'sorting':true,'asc':false,'desc':false}},
+                    {'field':'created_at','label':'Created at','title':false,'sortable':true,'html':true,'rowspan':null,'colspan':null,'class':{'sorting':true,'asc':false,'desc':true}},
+                    {'field':'actions','label':'Actions','sortable':false,'html':true,'rowspan':null,'colspan':null,'class':{'sorting':false,'asc':false,'desc':false}}
+                ],
+                localization: {
+                    "search": "Search",
+                    "next": "Next",
+                    "first": "First",
+                    "last": "Last",
+                    "prev": "Previous",
+                    "per_page": "Show",
+                    "all": "All",
+                    "empty": "No matching records found",
+                    "of": "of",
+                    "loading": "Loading..."
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .tooltip {
+        display: block !important;
+        z-index: 10000;
+
+    .tooltip-inner {
+        background: black;
+        color: white;
+        border-radius: 16px;
+        padding: 5px 10px 4px;
+    }
+
+    .tooltip-arrow {
+        width: 0;
+        height: 0;
+        border-style: solid;
+        position: absolute;
+        margin: 5px;
+        border-color: black;
+        z-index: 1;
+    }
+
+    &[x-placement^="top"] {
+         margin-bottom: 5px;
+
+    .tooltip-arrow {
+        border-width: 5px 5px 0 5px;
+        border-left-color: transparent !important;
+        border-right-color: transparent !important;
+        border-bottom-color: transparent !important;
+        bottom: -5px;
+        left: calc(50% - 5px);
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+    }
+
+    &[x-placement^="bottom"] {
+         margin-top: 5px;
+
+    .tooltip-arrow {
+        border-width: 0 5px 5px 5px;
+        border-left-color: transparent !important;
+        border-right-color: transparent !important;
+        border-top-color: transparent !important;
+        top: -5px;
+        left: calc(50% - 5px);
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+    }
+
+    &[x-placement^="right"] {
+         margin-left: 5px;
+
+    .tooltip-arrow {
+        border-width: 5px 5px 5px 0;
+        border-left-color: transparent !important;
+        border-top-color: transparent !important;
+        border-bottom-color: transparent !important;
+        left: -5px;
+        top: calc(50% - 5px);
+        margin-left: 0;
+        margin-right: 0;
+    }
+    }
+
+    &[x-placement^="left"] {
+         margin-right: 5px;
+
+    .tooltip-arrow {
+        border-width: 5px 0 5px 5px;
+        border-top-color: transparent !important;
+        border-right-color: transparent !important;
+        border-bottom-color: transparent !important;
+        right: -5px;
+        top: calc(50% - 5px);
+        margin-left: 0;
+        margin-right: 0;
+    }
+    }
+
+    &.popover {
+         $color: #f9f9f9;
+
+    .popover-inner {
+        background: $color;
+        color: black;
+        padding: 24px;
+        border-radius: 5px;
+        box-shadow: 0 5px 30px rgba(black, .1);
+    }
+
+    .popover-arrow {
+        border-color: $color;
+    }
+    }
+
+    &[aria-hidden='true'] {
+         visibility: hidden;
+         opacity: 0;
+         transition: opacity .15s, visibility .15s;
+     }
+
+    &[aria-hidden='false'] {
+         visibility: visible;
+         opacity: 1;
+         transition: opacity .15s;
+     }
+    }
+</style>
---
+++ src/TablePagination.vue
@@ -0,0 +1,185 @@
+<template>
+    <div class="d-flex justify-content-between align-items-center mt-2">
+        <div>{{ infoText }}</div>
+        <ul class="pagination">
+            <li class="page-item"
+                :class="{ disabled: prevIsDisabled }"
+            >
+                <a href="#" class="page-link"
+                   @click.prevent="firstPage"
+                   v-tooltip="labels.first"
+                >
+                    <<<
+                </a>
+            </li>
+            <li class="page-item"
+                :class="{ disabled: prevIsDisabled }"
+            >
+                <a href="#" class="page-link"
+                   @click.prevent="previousPage"
+                   v-tooltip="labels.prev"
+                >
+                    <
+                </a>
+            </li>
+            <li class="page-item" v-for="page in prevPages" :key="page">
+                <a href="#" class="page-link"
+                   @click.prevent="changePage(page)"
+                >
+                    {{ page }}
+                </a>
+            </li>
+            <li class="page-item active">
+                <a href="#" class="page-link"
+                   @click.prevent=""
+                >
+                    {{ currentPage }}
+                </a>
+            </li>
+            <li class="page-item" v-for="page in nextPages" :key="page">
+                <a href="#" class="page-link"
+                   @click.prevent="changePage(page)"
+                >
+                    {{ page }}
+                </a>
+            </li>
+            <li class="page-item"
+                :class="{ disabled: nextIsDisabled }"
+            >
+                <a class="page-link" href="#"
+                   @click.prevent="nextPage"
+                   v-tooltip="labels.next"
+                >
+                    >
+                </a>
+            </li>
+            <li class="page-item"
+                :class="{ disabled: nextIsDisabled }"
+            >
+                <a class="page-link" href="#"
+                   @click.prevent="lastPage"
+                   v-tooltip="labels.last"
+                >
+                    >>>
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
+​
+<script>
+    export default {
+        name: "TablePagination",
+        props: {
+            total_records: {
+                type: Number,
+                default: 0
+            },
+            current_page: {
+                type: Number,
+                default: 1,
+            },
+            current_per_page:{
+                type: Number,
+                default: 10,
+            },
+            labels: {
+                type: Object,
+                default: () => {}
+            }
+        },
+        data() {
+            return {
+                currentPage: this.current_page,
+                currentPerPage: this.current_per_page,
+                totalRecords: this.total_records,
+            }
+        },
+        watch:{
+            current_page(){
+                if (this.current_page < 1){
+                    this.firstPage()
+                } else {
+                    this.changePage(this.current_page);
+                }
+            },
+            current_per_page(){
+                this.currentPerPage = this.current_per_page
+            },
+            total_records(){
+                this.totalRecords = this.total_records;
+                if (this.current_page > this.pagesCount){
+                    this.lastPage()
+                }
+            },
+            currentPage(){
+                this.pageChanged()
+            }
+        },
+        computed: {
+            infoText(){
+                return this.itemStart + ' - ' + this.itemEnd + ' ' + this.labels.of + ' ' + this.totalRecords
+            },
+            prevPages(){
+                let length = this.currentPage > 3 ? 2 : this.currentPage - 1;
+                return Array.from({ length: length }, (v, k) => this.currentPage - k - 1).reverse();
+            },
+            nextPages(){
+                let length = (this.pagesCount - this.currentPage) > 2 ? 2 : this.pagesCount - this.currentPage;
+                return Array.from({ length: length }, (v, k) => this.currentPage + k + 1);
+            },
+            nextIsDisabled() {
+                return this.currentPage === this.pagesCount;
+            },
+            prevIsDisabled() {
+                return this.currentPage === 1;
+            },
+            pagesCount() {
+                return Math.ceil(this.totalRecords / this.currentPerPage);
+            },
+            itemStart() {
+                if (this.totalRecords > 0) {
+                    if (this.currentPage === 1) {
+                        return 1;
+                    } else {
+                        return this.currentPage*this.currentPerPage - this.currentPerPage + 1;
+                    }
+                } else {
+                    return 0;
+                }
+            },
+            itemEnd() {
+                if (this.totalRecords < this.itemStart + this.currentPerPage - 1 ){
+                    return this.totalRecords;
+                } else if (this.totalRecords > 0) {
+                    return this.itemStart + this.currentPerPage - 1
+                } else {
+                    return 0;
+                }
+            },
+        },
+        methods: {
+            nextPage() {
+                this.changePage(this.currentPage + 1);
+            },
+            previousPage() {
+                this.changePage(this.currentPage - 1);
+            },
+            firstPage() {
+                this.changePage(1);
+            },
+            lastPage() {
+                this.changePage(this.pagesCount);
+            },
+            changePage(number){
+                this.currentPage = number;
+            },
+            pageChanged() {
+                this.$emit('page-changed', {currentPage: this.currentPage});
+            },
+        }
+    }
+</script>
+​
+<style scoped>
+</style>
